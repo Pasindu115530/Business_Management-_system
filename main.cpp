e44f0a3e4cc0a7f1b18c8384e5e4e243b2211656
Pasindu115530 <pasinduudana12m2@gmail.com>
2024-09-09 14:37:48 +0530

    Add main cpp file with main funtions

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+void adminSignup(){}
+void userSignup(){}
+void adminLogin(){}
+void userLogin(){}
+void addItem(){}
+void showItem(){}
+void buyItem(){}
+void updateItem(){}
+void searchItem(){}
+
+int main(){
+
+    return 0;
+}
