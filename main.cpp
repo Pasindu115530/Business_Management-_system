414fb12cdfe7a57366e22699c7f653212878787c
Pasindu115530 <pasinduudana12m2@gmail.com>
2024-09-09 20:05:12 +0530

    add nuw funtion to easy do.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ struct user{
     string name;
     string password;
 };
-
 void loadUsers(user userlist[], int &count, int max) {
     fstream myfile;
     myfile.open("store.txt" , ios::in);
@@ -20,7 +19,6 @@ void loadUsers(user userlist[], int &count, int max) {
         myfile.close();
     }
 }
-
 void saveUser(const user& newUser) {
     fstream myfile("store.txt", ios::app); // 'app' mode to append data
     if (myfile.is_open()) {
@@ -30,8 +28,6 @@ void saveUser(const user& newUser) {
         cout << "Error: Unable to open file for writing." << endl;
     }
 }
-
-
 void userSignup(user userlist[] , int max){
     int count = 0;
     string name2,password2;
@@ -90,11 +86,13 @@ void userLogin(user userlist[],int max){
         }
         }
 }
+void adminShow();
+void customerDatabase(){}
+void userShow();
 void addItem(){}
-void showItem(){}
-void buyItem(){}
-void updateItem(){}
-void searchItem(){}
+void stock(){}
+void neworder(){}
+void orderdetail(){}
 
 int main(){
     int cho;  
@@ -102,6 +100,7 @@ int main(){
     user userlist[max];
     int count = 0;
     loadUsers(userlist, count, max);
+    int x;
 
 
  do{       
@@ -111,24 +110,93 @@ int main(){
     cout << " 01 . Customer Login  " << endl;
     cout << " 02 . Customer Signup " << endl;
     cout << " 03 . Admin Login     " << endl;
+    cout << " 04.exit" << endl;
     cout << " Please Your Choice  :  " ;
     cin >> cho ;
 
     switch(cho){
         case 1:
             userLogin(userlist ,  max);
+            x = 0;
             break;
         case 2:
             userSignup(userlist , max);
             break;
         case 3:
             adminLogin();
+            x = 5;
             break;
-                
+        case 4:
+            cout << "Exit "    ;
+            break;
     }
- }while(cho !=4);
+ }while(cho != 4 && x == 5 && x == 0);
+
+    if(x == 5){
+        adminShow();    
+    }
+    else if(x == 0){
+        userShow();
+
+
+    }
 
 
 
     return 0;
+}
+
+void adminShow(){
+    int choice;
+    cout << "01.Add items" << endl;
+    cout << "02.Show stocks" << endl;
+    cout << "03.Customer details" <<endl;
+    cout << "04.new orders" << endl;
+    cout << "05.Exit main menu" << endl;
+    cout << "06.exit " << endl;
+    cin >> choice;
+    switch(choice){
+        case 1:
+            addItem();
+            break;
+        case 2:
+            stock();
+            break;
+        case 3:
+            customerDatabase();
+            break;
+        case 4:
+            neworder();
+               
+
+
+    }
+
+
+}
+
+void userShow(){
+    int choice;
+    do{
+    cout << "01.Show items list" << endl;
+    cout << "02.Order details" << endl;
+    cout << "03.Exit main menu" << endl;
+    cout << "04.exit " << endl;
+    cin >> choice;  
+       switch(choice){
+        case 1:
+            userShow();
+            break;
+        case 2:
+            orderdetail();
+            break;
+        case 3:
+            break; //delop soon
+        case 4:
+            exit;
+               
+
+
+    } 
+    }while(choice != 3);
 }
